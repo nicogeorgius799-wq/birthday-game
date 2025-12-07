d4c0553bd2433cfab759d53499f05e44537ae254
nicogeorgius799-wq <nicogeorgius799@gmail.com>
2025-12-07 15:55:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const scoreElement = document.getElementById('score');
 
 // --- BILDDATEIEN (PLATZHALTER) ---
 // ERSETZE DIESE DURCH DEINE ECHTEN DATEINAMEN!
-const OBSTACLE_DOG_IMG_SRC = 'Hund.jpg'; // z.B. 'hund.png'
+const OBSTACLE_DOG_IMG_SRC = 'Hund.png'; // z.B. 'hund.png'
 const OBSTACLE_TIRE_IMG_SRC = 'Reifen.png'; // z.B. 'reifen.png'
 const BACKGROUND_IMG_SRC = 'Straße.jpg'; // z.B. 'strasse.jpg'
 
@@ -216,6 +216,7 @@ function drawStartScreen() {
 
 drawStartScreen();
 setInterval(createObstacle, OBSTACLE_SPAWN_RATE);
+
 
 
 
